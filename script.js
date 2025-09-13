ab5e73f117a12c79e3cdf9156d171790a606d15a
CastielWinchster <luishenriquecostamachado@outlook.com>
2025-09-12 23:24:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// Animação no botão
+document.querySelector(".btn-grad").addEventListener("mouseover", () => {
+  console.log("Hover no botão!");
+});
+
+// Função de Busca
+document.addEventListener('DOMContentLoaded', function () {
+  const input = document.getElementById('searchInput');
+  const clearIcon = document.querySelector('.clear-icon');
+
+  function toggleClearIcon() {
+    if (input.value.length > 0) {
+      clearIcon.style.display = 'block';
+    } else {
+      clearIcon.style.display = 'none';
+    }
+  }
+
+  clearIcon.addEventListener('click', function () {
+    input.value = '';
+    input.focus();
+    toggleClearIcon();
+  });
+
+  input.addEventListener('input', toggleClearIcon);
+
+  // Inicializa o estado do ícone
+  toggleClearIcon();
+});
+
+// Loading Screen
+document.addEventListener('DOMContentLoaded', function () {
+  // ...existing code...
+
+  // Loading screen
+  const loadingScreen = document.getElementById('loading-screen');
+  window.onload = function () {
+    setTimeout(function () {
+      loadingScreen.classList.add('hide');
+    }, 5000); // tempo em ms (1.2s)
+  };
+});
